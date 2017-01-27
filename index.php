c292d2261c2ad1f6d5eff25ce7c9b68ed145b55a
T.J.K <tjk16384@gmail.com>
2017-01-26 20:50:56 -0500

    Added building blocks
    Index.php, Bootstrap, jQuery, Tether & Normalize

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Assignment 1: Basic PHP CRUD</title>
+        
+        <!-- <link rel="stylesheet" href="./css/normalize.min.css" /> -->
+        <link rel="stylesheet" href="./css/bootstrap.min.css" />
+        
+        <script src="./js/jquery.slim.min.js"></script>
+        <script src="./js/tether.min.js"></script>
+        <script src="./js/bootstrap.min.js"></script>
+    </head>
+    
+    <body>
+        
+        <?php
+        $str = "Hello world!";
+        echo $str;
+        ?>
+        
+    </body>
+</html>
